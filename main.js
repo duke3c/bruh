0d9515edbadd00667c429e1bb8a4ca45e6662dc1
drip <82082642+duke3c@users.noreply.github.com>
2022-05-04 17:41:46 +0000

    removed comments

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const hbs = require('hbs');
 
-// Initializing the app.
 const app = express();
 
 app.use(express.urlencoded({extended:true}))
 
 app.set('view engine', 'hbs');
 
-// Getting the path request and sending the response with text
+
 const index = require('./routes/index.js')
 const bruh = require('./routes/bruh.js')
 
 app.use('/', index);
 app.use('/bruh', bruh);
 
-// Listen on port 2000
-app.listen(2000, ()=>{
-    console.log('listening at http://localhost:2000');
+app.listen(3000, ()=>{
+    console.log('listening at http://localhost:3000');
 })
